e99ce16a7e2026bb949d62465f1fb5ef774ed665
Alex Tang <alextang3@outlook.com>
2016-07-06 16:43:50 -0700

    fixed bug!

--- app.js
+++ app.js
@@ -1,29 +1,74 @@
-var app = angular.module('meanForum', ['ui.router']);
-app.factory('posts', [function(){
-    var o = {
-      posts: []
-    };
-    return o;
+// angular.module('meanForum', ['ui.router']);
+var meanForum = angular.module('meanForum', ['ui.router']);
+
+meanForum.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
+		$urlRouterProvider.otherwise('home');
 }]);
 
-app.controller('MainCtrl', [
+meanForum.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}]);
+
+meanForum.controller('MainCtrl', [
 '$scope',
 'posts',
 function($scope, posts){
-  $scope.posts = posts.posts;
-
+	$scope.posts = posts.posts;
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') { return; }
     $scope.posts.push({
       title: $scope.title,
-      upvotes: 0
+      link: $scope.link,
+      upvotes: 0,
+      comments: [
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+      ]
     });
     $scope.title = '';
     $scope.link = '';
   };
+	$scope.incrementUpvotes = function(post){
+		post.upvotes +=1;
+	};
+	$scope.decrementUpvotes = function(post){
+		post.upvotes -=1;
+	};
+}])
 
-  $scope.incrementUpvotes = function(post) {
-    post.upvotes += 1;
+meanForum.controller('PostsCtrl',
+['$scope', 'posts', '$stateParams', function ($scope, posts, $stateParams) {
+  $scope.post = posts.posts[$stateParams.id];
+  $scope.incrementUpvotes = function (comment) {
+    comment.upvotes += 1;
   };
-
+  $scope.addComment = function() {
+    if ($scope.body === "") {
+      return;
+    }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  };
 }]);
