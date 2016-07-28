2777888e59635df4d2bb51602a69061a150bb5c4
Alex Tang <alextang3@outlook.com>
2016-07-27 19:52:48 -0700

    modify angularApp.js

--- meanforum/public/javascripts/angularApp.js
+++ meanforum/public/javascripts/angularApp.js
@@ -30,6 +30,56 @@ function($stateProvider, $urlRouterProvider) {
 		$urlRouterProvider.otherwise('home');
 }]);
 
+meanForum.factory('auth', ['$http', '$window', function($http, $window) {
+  var auth = {};
+
+  auth.saveToken = function(token) {
+    $window.localStorage['meanForum-token'] = token;
+  };
+
+  auth.getToken = function() {
+    return $window.localStorage['meanForum-token'];
+  }
+
+  auth.isLoggedIn = function() {
+    var token = auth.getToken();
+
+    if(token) {
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+      return payload.exp > Date.now() / 1000;
+    } else {
+      return false;
+    }
+  };
+
+  auth.currentUser = function() {
+    if(auth.isLoggedIn()) {
+      var token = auth.getToken();
+      var payload = JSON.parse($window.atob(token.split('.')));
+
+      return payload.username;
+    }
+  };
+
+  auth.register = function() {
+    return $http.post('/register', user).success(function(data){
+      auth.saveToken(data.token);
+    });
+  };
+
+  auth.logIn = function(user) {
+    return $http.post('/login', user).success(function(data) {
+      auth.saveToken(data.token);
+    });
+  };
+
+  auth.logOut = function() {
+    $window.localStorage.removeItem('meanForum-token');
+  };
+
+  return auth;
+}])
+
 meanForum.factory('posts', ['$http', function($http){
   var o = {
     posts: []
