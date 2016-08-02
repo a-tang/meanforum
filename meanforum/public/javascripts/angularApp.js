e89e3da9eb51405532b31ff92f096cd5b873076e
Alex Tang <alextang3@outlook.com>
2016-08-01 23:01:50 -0700

    add navigation

--- meanforum/public/javascripts/angularApp.js
+++ meanforum/public/javascripts/angularApp.js
@@ -207,3 +207,12 @@ meanForum.controller('AuthCtrl', [
       });
     };
   }])
+
+  meanForum.controller('NavCtrl', [
+    '$scope',
+    'auth',
+    function($scope, auth) {
+      $scope.isLoggedIn = auth.isLoggedIn;
+      $scope.currentUser = auth.currentUser;
+      $scope.logOut = auth.logOut;
+    }]);
