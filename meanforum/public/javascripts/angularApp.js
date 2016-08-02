e80d911d0655a260f14e01e17725a535785cb7e1
Alex Tang <alextang3@outlook.com>
2016-08-01 23:15:39 -0700

    add post author display

--- meanforum/public/javascripts/angularApp.js
+++ meanforum/public/javascripts/angularApp.js
@@ -100,7 +100,7 @@ meanForum.factory('auth', ['$http', '$window', function($http, $window) {
   return auth;
 }])
 
-meanForum.factory('posts', ['$http', function($http){
+meanForum.factory('posts', ['$http', 'auth', function($http, auth) {
   var o = {
     posts: []
   };
@@ -110,13 +110,16 @@ meanForum.factory('posts', ['$http', function($http){
     });
   };
   o.create = function(post) {
-    return $http.post('/posts', post).success(function(data){
+    return $http.post('/posts', post, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    }).success(function(data){
       o.posts.push(data);
     });
   };
   o.upvote = function(post) {
-    return $http.put('/posts/' + post._id + '/upvote')
-    .success(function(data){
+    return $http.put('/posts/' + post._id + '/upvote', null, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    }).success(function(data){
       post.upvotes += 1;
     });
   };
@@ -126,11 +129,14 @@ meanForum.factory('posts', ['$http', function($http){
     });
   };
   o.addComment = function(id, comment) {
-    return $http.post('/posts/' + id + '/comments', comment);
+    return $http.post('/posts/' + id + '/comments', comment, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    });
   };
   o.upvoteComment = function(post, comment) {
-    return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
-      .success(function(data){
+    return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote', null, {
+      headers: {Authorization: 'Bearer '+auth.getToken()}
+    }).success(function(data){
         comment.upvotes += 1;
       });
   };
